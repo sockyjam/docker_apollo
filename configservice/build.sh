766bcb137771dea8c8f7d7f87e0ce1cba1c7f379
root@lookstore <rootlookstore@loookauto.com>
2020-11-17 15:13:01 +0800

    update, ct.

--- configservice/build.sh
+++
@@ -1,9 +0,0 @@
-SERVER_PORT_DEV=8080
-SERVER_PORT_TEST=8082
-SERVER_PORT_PRO=8083
-
-export SERVER_PORT=8080
-export HOST_IP=192.168.11.61
-
-docker build -t sockyjam/apollo.configservice.dev:v02 .
-
